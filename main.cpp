e74f79559b74edd8ca1e8c9b62388e59bf5bc538
Piotr <piotr.wasilewski.programista@gmail.com>
2022-08-14 18:08:32 +0200

    prepared main class to test others file

--- main.cpp
+++ main.cpp
@@ -8,48 +8,53 @@ using namespace std;
 int _main()
 {
 
-    CMarkup xml;
-
-    bool fileExists = xml.Load( "users.xml" );
-
-    if (!fileExists)
-    {
-        xml.SetDoc("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n");
-        xml.AddElem("Users");
-    }
-
-    xml.FindElem();
-    xml.IntoElem();
-    xml.AddElem("User");
-    xml.IntoElem();
-    xml.AddElem("UserId", "2");
-    xml.AddElem("Login", "bartek");
-    xml.AddElem("Password", "456");
-
-    xml.Save("users.xml");
-
     return 0;
 }
+
 
 #include "FileWithIncomes.h"
-int main()
-{
-    FileWithIncomes fileWithIncomes("fileWithIncomes.xml");
-    cout<< fileWithIncomes.isFileEmpty()<<endl;
-    fileWithIncomes.saveIncomesToFile();
+#include "Income.h"
+#include "Calendar.h"
+#include "Markup.h"
+//#include <span class="code-string">"Markup.h"</span>
 
+int fileWithIncomes_main()
+{
+    FileWithIncomes fileWithIncomes("fileWithIncomes.xml",707);
+        //cout<< fileWithIncomes.isFileEmpty()<<endl;
+        // fileWithIncomes.saveIncomesToFile();
+     //    fileWithIncomes.deleteIncome();
+        //fileWithIncomes.addIncome();
+ //fileWithIncomes.addIncome(); fileWithIncomes.addIncome();
+           //Income income(fileWithIncomes.getNewIncomeData());
+          // fileWithIncomes.saveIncomeToFile();
+            //income.display();
+
+       // Calendar calendar;
+        //struct tm date = Calendar::getCurrentDate();
+       // cout<<"Data: "<<date.tm_year << "/" << date.tm_mon << "/" << date.tm_mday <<endl;
     return 0;
 }
 
+#include "FileWithExpenses.h"
 #include "Income.h"
-int Income_main()
+#include "Calendar.h"
+#include "Markup.h"
+//#include <span class="code-string">"Markup.h"</span>
+
+int main()
 {
-    Income income(10,500,200,03,185);
-    income.display();
+    FileWithExpenses fileWithExpenses("fileWithExpenses.xml",707);
+        //cout<< fileWithIncomes.isFileEmpty()<<endl;
+        // fileWithIncomes.saveIncomesToFile();
+     //    fileWithIncomes.deleteIncome();
+        //fileWithIncomes.addIncome();
+ fileWithExpenses.addExpense(); fileWithExpenses.addExpense(); fileWithExpenses.addExpense();
+          // fileWithIncomes.saveIncomeToFile();
+            //income.display();
 
-    income.setData(0,1,1);
-    income.setIncome(2000);
-    income.display();
-
+       // Calendar calendar;
+        //struct tm date = Calendar::getCurrentDate();
+       // cout<<"Data: "<<date.tm_year << "/" << date.tm_mon << "/" << date.tm_mday <<endl;
     return 0;
 }
