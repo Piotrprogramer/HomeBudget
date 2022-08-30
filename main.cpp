5821adefb25d00ced8549bb65919fc5f3dbf20bb
Piotr <piotr.wasilewski.programista@gmail.com>
2022-08-30 22:52:50 +0200

    prepare main class to testing other class

--- main.cpp
+++ main.cpp
@@ -18,43 +18,11 @@ int _main()
 #include "Markup.h"
 //#include <span class="code-string">"Markup.h"</span>
 
-int fileWithIncomes_main()
-{
-    FileWithIncomes fileWithIncomes("fileWithIncomes.xml",707);
-        //cout<< fileWithIncomes.isFileEmpty()<<endl;
-        // fileWithIncomes.saveIncomesToFile();
-     //    fileWithIncomes.deleteIncome();
-        //fileWithIncomes.addIncome();
- //fileWithIncomes.addIncome(); fileWithIncomes.addIncome();
-           //Income income(fileWithIncomes.getNewIncomeData());
-          // fileWithIncomes.saveIncomeToFile();
-            //income.display();
-
-       // Calendar calendar;
-        //struct tm date = Calendar::getCurrentDate();
-       // cout<<"Data: "<<date.tm_year << "/" << date.tm_mon << "/" << date.tm_mday <<endl;
-    return 0;
-}
-
-#include "FileWithExpenses.h"
-#include "Income.h"
-#include "Calendar.h"
-#include "Markup.h"
-//#include <span class="code-string">"Markup.h"</span>
-
 int main()
 {
-    FileWithExpenses fileWithExpenses("fileWithExpenses.xml",707);
-        //cout<< fileWithIncomes.isFileEmpty()<<endl;
-        // fileWithIncomes.saveIncomesToFile();
-     //    fileWithIncomes.deleteIncome();
-        //fileWithIncomes.addIncome();
- fileWithExpenses.addExpense(); fileWithExpenses.addExpense(); fileWithExpenses.addExpense();
-          // fileWithIncomes.saveIncomeToFile();
-            //income.display();
+    FileWithIncomes fileWithIncomes("fileWithIncomes.xml",707);
+    fileWithIncomes.getVectorWithIncomesOfDateRange();
 
-       // Calendar calendar;
-        //struct tm date = Calendar::getCurrentDate();
-       // cout<<"Data: "<<date.tm_year << "/" << date.tm_mon << "/" << date.tm_mday <<endl;
     return 0;
 }
+
